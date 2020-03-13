be402b7ed19fdbf938a26796583f0d3123b68877
Roshan Jambhavdekar <roshan.j@vserv.com>
2020-03-14 04:41:52 +0530

    Updates

---
+++ precache-manifest.7ec0ebb4fd394c803accb98c890103ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258790bb95faa846885a7866d5ec5009",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "201787d5e33c01f5046e",
+    "url": "/reactjs/static/css/main.96406e59.chunk.css"
+  },
+  {
+    "revision": "83e2f5d9d872002049b8",
+    "url": "/reactjs/static/js/2.2df2d9eb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactjs/static/js/2.2df2d9eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "201787d5e33c01f5046e",
+    "url": "/reactjs/static/js/main.84f0ed37.chunk.js"
+  },
+  {
+    "revision": "5552f0042e1dc1604bd6",
+    "url": "/reactjs/static/js/runtime-main.2be654e6.js"
+  }
+]);
